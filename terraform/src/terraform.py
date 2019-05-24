a8df378331dc4a8bee0c6bfa6c52681db77ac0e4
Raymond Butcher <ray.butcher@claranet.uk>
2019-05-24 09:23:36 +0100

    Improve file creation, add single file example

--- terraform/src/terraform.py
+++ terraform/src/terraform.py
@@ -1,13 +1,11 @@
 from pretf import aws, tf, register_file
 
 
-@register_file('terraform.json')
+@register_file('terraform.tf.json')
 def terraform_block(params):
 
     backend_name = f'customer-tfstate-{params.envtype}'
 
-    # This will prompt to create the bucket/table if they don't exist.
-    # Try to use cloudformation?
     backend = aws.create_s3_backend(
         region=params.region,
         bucket=backend_name,
