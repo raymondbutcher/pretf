70f2fbb28df360139123ed1b7d4820f9106f5301
Raymond Butcher <randomy@gmail.com>
2019-05-25 18:48:24 +0100

    Use pretf-aws in tests

--- tests/custom/prod/pretf.py
+++ tests/custom/prod/pretf.py
@@ -3,7 +3,12 @@ from pretf import create, execute, remove
 
 def run():
 
-    params = {"aws_region": "eu-west-1", "envname": "prod", "envtype": "prod"}
+    params = {
+        "aws_profile": "rbutcher",
+        "aws_region": "eu-west-1",
+        "envname": "prod",
+        "envtype": "prod",
+    }
 
     created = create("../src", **params)
 
--- tests/custom/src/aws.tf.py
+++ tests/custom/src/aws.tf.py
@@ -1,18 +1,50 @@
 from pretf import tf
+from pretf_aws import get_frozen_credentials
 
-
-def terraform(aws_region, **kwargs):
+def terraform(aws_profile, aws_region, **kwargs):
     """
     This function would normally be a generator,
     but it can also be a regular function that returns a list.
 
-    This function also demonstrates returning a standard dictionary
+    This also demonstrates returning a standard dictionary
     instead of using the tf object.
+
+    This also shows how to use pretf_aws to get AWS credentials
+    and use them in multiple providers. This allows for a single
+    Terraform stack to manage resources in multiple accounts.
+    However, in this test I am using the same account twice.
+
+    The result of this file (aws.tf.json) is in .gitignore
+    to avoid publishing AWS credentials.
 
     """
 
-    tf_block = tf("provider.aws", {"region": aws_region})
+    # Normally you would not need to use creds here,
+    # and rely on environment variables set by terraform.tf.py
+    # but this shows how to work with multiple AWS accounts.
 
-    dict_block = {"provider": {"aws": {"alias": "london", "region": "eu-west-2"}}}
+    creds = get_frozen_credentials(profile_name=aws_profile)
+
+    tf_block = tf(
+        "provider.aws",
+        {
+            "region": aws_region,
+            "access_key": creds.access_key,
+            "secret_key": creds.secret_key,
+            "token": creds.token,
+        },
+    )
+
+    dict_block = {
+        "provider": {
+            "aws": {
+                "alias": "london",
+                "region": "eu-west-2",
+                "access_key": creds.access_key,
+                "secret_key": creds.secret_key,
+                "token": creds.token,
+            }
+        }
+    }
 
     return [tf_block, dict_block]
