f2c6c4abc69b21eb58ddf5b823a7458d572d1ec0
Raymond Butcher <randomy@gmail.com>
2019-06-08 16:44:54 +0100

    Generate JSON from collections

--- tests/test_collections.py
+++ tests/test_collections.py
@@ -7,29 +7,73 @@ from pretf.render import Block, VariableNotPopulated
 
 @collect
 def iam_user(var):
-
-    # Yield variable to define function inputs.
+    # Inputs.
     yield tf("variable.name")
     yield tf("variable.path", {"default": "/"})
 
-    # Yield regular resources to include in JSON.
+    # Resources.
     user = yield tf(
         f"resource.aws_iam_user.{var.name}", {"name": var.name, "path": var.path}
     )
 
-    # Yield outputs to define attributes.
-    yield tf("output.arn", {"value": user.arn})
-    yield tf("output.user", {"value": user})
-    yield tf("output.dict", {"value": {"anything": True}})
+    # Outputs.
+    yield tf("output.name", {"value": var.name})
+    yield tf("output.resource", {"value": user})
 
 
 @collect
-def iam_users(var):
+def iam_group(var):
+    # Inputs.
+    yield tf("variable.name")
+    yield tf("variable.path", {"default": "/"})
 
-    yield tf("variable.names")
+    # Resources.
+    group = yield tf(f"resource.aws_iam_group.{var.name}", {"name": var.name})
 
-    for name in var.names:
-        yield iam_user(name=name)
+    # Outputs.
+    yield tf("output.name", {"value": var.name})
+    yield tf("output.resource", {"value": group})
+
+
+@collect
+def iam_group_with_users(var):
+    # Inputs.
+    yield tf("variable.group_name")
+    yield tf("variable.user_names")
+
+    # Yield resources from a nested collection.
+    group = yield iam_group(name=var.group_name)
+
+    # Yield resources from a nested collection.
+    users = {}
+    for name in var.user_names:
+        user = yield iam_user(name=name)
+        users[name] = user.resource
+
+    # Yield resources from a nested collection,
+    # using "yield from" this time.
+    # It can be assigned to a variable this way.
+    yield from aws_iam_user_group_membership(group=group.resource, users=users)
+
+    # Outputs.
+    yield tf("output.group", {"value": group.resource})
+    yield tf("output.users", {"value": group.resource})
+
+
+@collect
+def aws_iam_user_group_membership(var):
+    # Inputs.
+    yield tf("variable.group")
+    yield tf("variable.users")
+
+    # Resources.
+    group_label = str(var.group).split(".")[-1]
+    for user_label, user in sorted(var.users.items()):
+        label = f"{user_label}_in_{group_label}"
+        yield tf(
+            f"resource.aws_iam_user_group_membership.{label}",
+            {"user": user.name, "groups": [var.group.name]},
+        )
 
 
 class TestCollections(unittest.TestCase):
@@ -44,27 +88,59 @@ class TestCollections(unittest.TestCase):
 
         # The collection is iterable and contains yielded blocks,
         # excluding variables and outputs.
-        blocks = list(peanut)
         expected = [
             {"resource": {"aws_iam_user": {"peanut": {"name": "peanut", "path": "/"}}}}
         ]
-        self.assertEqual(expected, blocks)
+        self.assertEqual(expected, list(peanut))
 
         # Yielded outputs can be accessed as attributes.
-        # This one is a string (attribute of a Block).
-        arn = peanut.arn
-        expected = "${aws_iam_user.peanut.arn}"
-        self.assertEqual(expected, arn)
+        # This one is a simple string..
+        self.assertEqual("peanut", peanut.name)
 
         # This one is a Block.
-        user = peanut.user
+        user = peanut.resource
         self.assertTrue(isinstance(user, Block))
-        expected = "${aws_iam_user.peanut.name}"
-        self.assertEqual(expected, user.name)
-
-        # This one is a dict.
-        self.assertEqual(peanut.dict, {"anything": True})
+        self.assertEqual(user.arn, "${aws_iam_user.peanut.arn}")
 
         # This one doesn't exist
         with self.assertRaises(AttributeError):
             peanut.nope
+
+    def test_nested_collections(self):
+
+        # Create a collection that has nested collections.
+        result = iam_group_with_users(
+            group_name="dogs", user_names=["peanut", "cornelius"]
+        )
+
+        # Check it created the resources from the nested collections.
+        expected = [
+            {"resource": {"aws_iam_group": {"dogs": {"name": "dogs"}}}},
+            {"resource": {"aws_iam_user": {"peanut": {"name": "peanut", "path": "/"}}}},
+            {
+                "resource": {
+                    "aws_iam_user": {"cornelius": {"name": "cornelius", "path": "/"}}
+                }
+            },
+            {
+                "resource": {
+                    "aws_iam_user_group_membership": {
+                        "cornelius_in_dogs": {
+                            "groups": ["${aws_iam_group.dogs.name}"],
+                            "user": "${aws_iam_user.cornelius.name}",
+                        }
+                    }
+                }
+            },
+            {
+                "resource": {
+                    "aws_iam_user_group_membership": {
+                        "peanut_in_dogs": {
+                            "groups": ["${aws_iam_group.dogs.name}"],
+                            "user": "${aws_iam_user.peanut.name}",
+                        }
+                    }
+                }
+            },
+        ]
+        self.assertEqual(expected, list(result))
