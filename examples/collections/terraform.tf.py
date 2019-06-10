e663aa258f41884b6b08586e054bdade9fb93566
Raymond Butcher <ray.butcher@claranet.uk>
2019-06-10 09:36:25 +0100

    Add S3 backend to collections example

---
+++ examples/collections/terraform.tf.py
@@ -0,0 +1,19 @@
+from pretf.api import tf
+from pretf.aws import get_session, terraform_s3_backend
+
+
+def terraform(var):
+
+    session = get_session(profile_name=var.aws_profile)
+
+    backend_name = f"pretf-tfstate-{var.envtype}"
+
+    yield terraform_s3_backend(
+        bucket=backend_name,
+        key="collections.tfstate",
+        region=var.aws_region,
+        session=session,
+        table=backend_name,
+    )
+
+    yield tf("terraform", {"required_version": "0.12.1"})
