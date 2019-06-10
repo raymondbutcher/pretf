de8634ba2fb52bfcd63c5ddc09d7bf3f642db57d
Raymond Butcher <randomy@gmail.com>
2019-06-10 09:26:54 +0100

    Add S3 backend to iam-users example

--- examples/iam-users/iam.tf.py
+++ examples/iam-users/iam.tf.py
@@ -3,13 +3,15 @@ from pretf.api import tf
 
 def terraform(var):
 
-    group = yield tf("resource.aws_iam_group.admins", {"name": "admins"})
+    group = yield tf("resource.aws_iam_group.pretf", {"name": "pretf-iam-users"})
 
-    for name in var.admin_user_names:
+    for name in var.user_names:
 
-        user = yield tf(f"resource.aws_iam_user.{name}", {"name": name})
+        name_lower = name.lower().replace("-", "_")
+
+        user = yield tf(f"resource.aws_iam_user.{name_lower}", {"name": name})
 
         yield tf(
-            f"resource.aws_iam_user_group_membership.{name}",
+            f"resource.aws_iam_user_group_membership.{name_lower}",
             {"user": user.name, "groups": [group.name]},
         )
--- examples/iam-users/variables.tf
+++ examples/iam-users/variables.tf
@@ -1,3 +1,15 @@
-variable "admin_user_names" {
-  default = ["ray", "violet"]
+variable "aws_profile" {
+  default = "rbutcher"
+}
+
+variable "aws_region" {
+  default = "eu-west-1"
+}
+
+variable "envtype" {
+  default = "nonprod"
+}
+
+variable "user_names" {
+  default = ["pretf-iam-user-1", "pretf-iam-user-2"]
 }
