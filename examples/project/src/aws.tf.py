e1586947aca46a6aeea24332e86360005a84b587
Raymond Butcher <randomy@gmail.com>
2019-06-11 18:50:51 +0100

    Add provider_aws function
    Also remove the unsupported check from the backend function because I don't really know that they're cause problems.

--- examples/project/src/aws.tf.py
+++ examples/project/src/aws.tf.py
@@ -1,43 +1,18 @@
 from pretf.api import tf
-from pretf.aws import get_frozen_credentials
+from pretf.aws import provider_aws
 
 
 def terraform(var):
     """
-    This shows how to use pretf_aws to get AWS credentials
-    and use them in multiple providers. This allows for a single
-    Terraform stack to manage resources in multiple accounts.
-    However, in this test I am using the same account twice.
-
-    This also demonstrates returning a standard dictionary
-    instead of using the tf object.
+    This shows how to use pretf_aws for multiple AWS providers.
+    This allows for a single Terraform stack to manage resources
+    in multiple accounts. However, in this test I am using the
+    same account twice.
 
     """
 
-    # Normally you would not need to use creds here,
-    # and rely on environment variables set by terraform.tf.py
-    # but this shows how to work with multiple AWS accounts.
+    yield provider_aws(profile=var.aws_profile, region=var.aws_region)
 
-    creds = get_frozen_credentials(profile_name=var.aws_profile)
+    london = yield provider_aws(profile=var.aws_profile, region="eu-west-1", alias="london")
 
-    yield tf(
-        "provider.aws",
-        {
-            "region": var.aws_region,
-            "access_key": creds.access_key,
-            "secret_key": creds.secret_key,
-            "token": creds.token,
-        },
-    )
-
-    yield {
-        "provider": {
-            "aws": {
-                "alias": "london",
-                "region": "eu-west-2",
-                "access_key": creds.access_key,
-                "secret_key": creds.secret_key,
-                "token": creds.token,
-            }
-        }
-    }
+    print(london.alias)
