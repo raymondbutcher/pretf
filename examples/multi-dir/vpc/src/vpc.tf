f9df24a0007f739bccc5776bc4aa7346351cdfa6
Raymond Butcher <ray.butcher@claranet.uk>
2019-06-13 18:27:14 +0100

    Delete multi-dir example
    It's like the flatten example but slightly worse

--- examples/multi-dir/vpc/src/vpc.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr_block
-  tags = {
-    Name        = "pretf-workspaces-multi-dir-${var.environment}"
-    Environment = var.environment
-  }
-}
