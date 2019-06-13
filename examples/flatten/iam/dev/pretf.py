d30e5573b0a17813712d67bc0f9620802d160a99
Raymond Butcher <randomy@gmail.com>
2019-06-13 21:04:25 +0100

    Add option to exclude directories from mirror_files

--- examples/flatten/iam/dev/pretf.py
+++ examples/flatten/iam/dev/pretf.py
@@ -2,5 +2,5 @@ from pretf import workflow
 
 
 def pretf_workflow():
-    workflow.mirror_files("../../*.*", "../*.*")
+    workflow.mirror_files("../../*", "../*", include_directories=False)
     return workflow.default()
