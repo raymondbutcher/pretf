4c1d91e55ee02ebdf58c1cc29a81c678d167b0b2
Raymond Butcher <ray.butcher@claranet.uk>
2019-06-14 10:24:26 +0100

    Improve the workspaces example

--- examples/workspaces/pretf.py
+++ examples/workspaces/pretf.py
@@ -1,5 +1,31 @@
+from os.path import relpath
+from pathlib import Path
+
 from pretf import workflow
+from pretf.api import log
 
 
-def pretf_workflow():
-    return workflow.deny()
+def pretf_workflow(path, terraform):
+    # Restrict where pretf/terraform can run to the stack directories.
+    stacks_dir = Path(__file__).parent / "stacks"
+    if path.cwd.parent != stacks_dir:
+        log.bad("you are not in a stack directory")
+        stack_dirs = [p for p in sorted(stacks_dir.iterdir()) if p.is_dir()]
+        if stack_dirs:
+            log.bad("found:")
+            for stack_dir in stack_dirs:
+                log.bad(f"* {relpath(stack_dir)}")
+        return 1
+
+    # Symlink stack.tf.py into the current directory,
+    # which handles the AWS provider and S3 backend.
+    # Also get the tfvars file for the current workspace.
+    stack = path.cwd.name
+    workspace = terraform.workspace
+    workflow.mirror_files(
+        "../stack.tf.py", f"../../params/{stack}.{workspace}.auto.tfvars"
+    )
+
+    # Now run the standard Pretf workflow which generates files
+    # and then executes Terraform.
+    return workflow.default()
--- examples/workspaces/vpc/pretf.py
+++
@@ -1,9 +0,0 @@
-from pretf import workflow
-
-
-def pretf_workflow(terraform):
-    workflow.mirror_files(
-        "../modules/stack/stack.tf.py",
-        f"../params/vpc.{terraform.workspace}.auto.tfvars",
-    )
-    return workflow.default()
