a18e24b321a697ffa511289e2edd5f6bddd3f4c1
Raymond Butcher <ray.butcher@claranet.uk>
2019-06-20 22:01:10 +0100

    Convert AWS example to pytest

--- examples/aws/terraform.tf.py
+++ examples/aws/terraform.tf.py
@@ -1,15 +1,11 @@
-from pretf.api import block
 from pretf.aws import terraform_backend_s3
 
 
 def pretf_blocks(var):
-
     yield terraform_backend_s3(
-        bucket=f"pretf-tfstate-example-aws-{var.aws_account_id}",
-        dynamodb_table="pretf-tfstate-example-aws",
-        key="terraform.tfstate",
+        bucket="pretf-tfstate-nonprod",
+        dynamodb_table="pretf-tfstate-nonprod",
+        key="examples/aws/terraform.tfstate",
         profile=var.aws_profile,
         region=var.aws_region,
     )
-
-    yield block("terraform", {"required_version": "0.12.1"})
---
+++ examples/aws/test_aws.py
@@ -0,0 +1,26 @@
+from unittest.mock import ANY
+
+from pretf import test, workflow
+
+
+class TestAWS(test.SimpleTest):
+    def test_init(self):
+        workflow.delete_files()
+        workflow.create_files()
+        self.init()
+
+    def test_outputs(self):
+
+        outputs = self.apply()
+
+        assert outputs == {
+            "private_sg_id": ANY,
+            "public_sg_id": ANY,
+            "total_bytes": 63,
+            "total_files": 5,
+            "user_pretf_iam_user_1": "pretf-iam-user-1",
+            "user_pretf_iam_user_2": "pretf-iam-user-2",
+        }
+
+    def test_destroy(self):
+        self.destroy()
