77ef2f69681556a8a05dc584b8582994fc6e8c63
Raymond Butcher <ray.butcher@claranet.uk>
2019-07-05 09:27:00 +0100

    Use labels helper and reformat without black

--- examples/aws/iam.tf.py
+++ examples/aws/iam.tf.py
@@ -1,21 +1,25 @@
-from pretf.api import block
+from pretf.api import block, labels
 
 
 def pretf_blocks(var):
 
-    group = yield block("resource", "aws_iam_group", "pretf", {"name": "pretf-aws"})
+    group = yield block("resource", "aws_iam_group", "pretf", {
+        "name": "pretf-aws",
+    })
 
     for name in var.user_names:
 
-        name_label = name.lower().replace("-", "_")
+        name_label = labels.clean(name)
 
-        user = yield block("resource", "aws_iam_user", name_label, {"name": name})
+        user = yield block("resource", "aws_iam_user", name_label, {
+            "name": name,
+        })
 
-        yield block(
-            "resource",
-            "aws_iam_user_group_membership",
-            name_label,
-            {"user": user.name, "groups": [group.name]},
-        )
+        yield block("resource", "aws_iam_user_group_membership", name_label, {
+            "user": user.name,
+            "groups": [group.name],
+        })
 
-        yield block("output", f"user_{name_label}", {"value": user.name})
+        yield block("output", f"user_{name_label}", {
+            "value": user.name,
+        })
