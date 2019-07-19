201d3af7fa190dddadfe213af55b4904943b60aa
Pierre Mdawar <p.mdawar@gmail.com>
2019-07-19 19:43:58 +0300

    [tests] added tests for interpolations with indexes

--- tests/test_api.py
+++ tests/test_api.py
@@ -14,3 +14,13 @@ def test_provider_alias_default():
 def test_variable():
     one = block("variable", "one", {})
     assert str(one) == "${var.one}"
+
+
+def test_index_interpolation():
+    instance = block("resource", "aws_instance", "www", {}).ipv6_addresses[0]
+    assert str(instance) == "${aws_instance.www.ipv6_addresses[0]}"
+
+
+def test_nested_index_interpolation():
+    interpolated = block("resource", "one", "two", {}).list[0].another_list[1]
+    assert str(interpolated) == "${one.two.list[0].another_list[1]}"
