2e3b73f7e6f21095009c4dd449b02a76d6b6180f
Gabriele <neothematrix@users.noreply.github.com>
2020-03-03 13:32:25 +0100

    Fixed Windows compatibility (#43)

--- pretf/pretf/workflow.py
+++ pretf/pretf/workflow.py
@@ -208,7 +208,10 @@ def execute_terraform(verbose: bool = True) -> CompletedProcess:
     # Find the Terraform executable in the PATH.
     for path in os.environ["PATH"].split(os.pathsep):
 
-        terraform_path = os.path.join(path, "terraform")
+        if sys.platform == "win32":
+            terraform_path = os.path.join(path, "terraform.exe")
+        else:
+            terraform_path = os.path.join(path, "terraform")
 
         # Skip if it doesn't exist here.
         if not os.path.exists(terraform_path):
