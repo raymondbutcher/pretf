9efc3ce7a52ef042f59e2fc42c404096a64bbfb1
Raymond Butcher <randomy@gmail.com>
2020-03-03 08:05:37 +0000

    Fix provider references

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,31 +1,28 @@
+import pytest
+
 from pretf.api import block
 
 
-def test_provider_alias():
-    default = block("provider", "aws", {"alias": "nonprod"})
-    assert str(default.alias) == "aws.nonprod"
-
-
-def test_provider_alias_default():
-    default = block("provider", "aws", {})
-    assert str(default.alias) == "aws.default"
-
-
-def test_variable():
-    one = block("variable", "one", {})
-    assert str(one) == "${var.one}"
-
-
-def test_resource():
-    resource = block("resource", "one", "two", {})
-    assert str(resource) == "${one.two}"
-
-
-def test_index_interpolation():
-    instance = block("resource", "aws_instance", "www", {}).ipv6_addresses[0]
-    assert str(instance) == "${aws_instance.www.ipv6_addresses[0]}"
-
-
-def test_nested_index_interpolation():
-    interpolated = block("resource", "one", "two", {}).list[0].another_list[1]
-    assert str(interpolated) == "${one.two.list[0].another_list[1]}"
+@pytest.mark.parametrize(
+    "obj,expected",
+    [
+        (block("provider", "aws", {}), "aws"),
+        (block("provider", "aws", {}).alias, "aws"),
+        (block("provider", "aws", {"region": "eu-west-1"}), "aws"),
+        (block("provider", "aws", {"region": "eu-west-1"}).alias, "aws"),
+        (block("provider", "aws", {"alias": "nonprod"}), "aws.nonprod"),
+        (block("provider", "aws", {"alias": "nonprod"}).alias, "aws.nonprod"),
+        (block("variable", "one", {}), "${var.one}"),
+        (block("resource", "one", "two", {}), "${one.two}"),
+        (
+            block("resource", "aws_instance", "www", {}).ipv6_addresses[0],
+            "${aws_instance.www.ipv6_addresses[0]}",
+        ),
+        (
+            block("resource", "one", "two", {}).list[0].another_list[1],
+            "${one.two.list[0].another_list[1]}",
+        ),
+    ],
+)
+def test_block(obj, expected):
+    assert str(obj) == expected
