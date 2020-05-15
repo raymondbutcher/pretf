5d348b123fda8e227c28c864f15e62f18332b0ce
Raymond Butcher <ray.butcher@claranet.uk>
2020-05-15 12:55:11 +0100

    Search for directories in get_outputs()
    No need to worry about the relative depth of the other stack.

--- examples/flatten/stacks/vpc-peering/vpc.auto.tfvars.py
+++ examples/flatten/stacks/vpc-peering/vpc.auto.tfvars.py
@@ -1,9 +1,15 @@
-from pretf.api import get_outputs
+from pretf.api import get_outputs, log
 
 
 def pretf_variables(var):
-    dev = get_outputs("../../vpc/dev")
-    prod = get_outputs("../../vpc/prod")
+    dev = get_outputs("vpc/dev")
+    if not dev:
+        raise log.bad("vpc/dev stack has no outputs")
+
+    prod = get_outputs("vpc/prod")
+    if not dev:
+        raise log.bad("vpc/prod stack has no outputs")
+
     yield {
         "dev_vpc_id": dev["vpc_id"],
         "prod_vpc_id": prod["vpc_id"],
--- pretf/pretf/api.py
+++ pretf/pretf/api.py
@@ -4,7 +4,6 @@ from typing import Any, Optional, Union
 
 from . import labels, log
 from .blocks import Block
-from .exceptions import FunctionNotFoundError
 from .util import is_verbose
 
 
@@ -21,31 +20,67 @@ def block(block_type: str, *args: Any) -> Block:
 def get_outputs(cwd: Union[Path, str], verbose: Optional[bool] = None) -> dict:
     """
     Runs `pretf output` in the specified directory and returns the values.
+    If the path is not anchored (i.e. does not start with ./ or ../ or /)
+    then it will check the current directory and all parent directories
+    until found.
 
     """
 
     from pretf.test import PretfProxy
 
-    if is_verbose(verbose):
+    if isinstance(cwd, Path):
+        # Use path as-is.
+        path = cwd
+    elif isinstance(cwd, str):
+        if cwd.startswith("./") or cwd.startswith("../") or cwd.startswith("/"):
+            # Use path as-is.
+            path = Path(cwd)
+        else:
+            # Look for this unanchored path in the current directory
+            # and all parent directories until found.
+            here = Path.cwd()
+            while True:
+                path = here / cwd
+                if path.is_dir():
+                    # Use this matching directory.
+                    break
+                elif here.parent == here:
+                    # Reached the top, use the path as a relative path
+                    # and let Terraform give an error.
+                    path = Path(cwd)
+                    break
+                else:
+                    # Move up a directory and let it try there.
+                    here = here.parent
+    else:
+        raise TypeError(cwd)
+
+    if is_verbose(verbose) or not path.is_dir():
 
         # Find the calling directory of this function, usually the directory
         # containing the pretf.workflow.py file that has called this function.
         frame = inspect.currentframe()
         if not frame:
-            raise FunctionNotFoundError(
-                "workflow: get_outputs() called from unknown frame"
-            )
+            raise Exception("get_outputs() called from unknown frame")
         caller_frame = frame.f_back
         if not caller_frame:
-            raise FunctionNotFoundError(
-                "workflow: get_outputs() called from unknown caller"
-            )
+            raise Exception("get_outputs() called from unknown caller")
         caller_info = inspect.getframeinfo(caller_frame)
         caller_file = caller_info.filename
 
-        log.ok(f"outputs: {cwd} -> {caller_file}")
+        if path.is_dir():
+            log.ok(f"outputs: {cwd} -> {caller_file}")
+        else:
+            if path.exists():
+                raise log.bad(
+                    f"get_outputs({cwd!r}) in {caller_file}: {path} is not a directory"
+                )
+            else:
+                raise log.bad(
+                    f"get_outputs({cwd!r}) in {caller_file}: {path} does not exist"
+                )
 
-    outputs = PretfProxy(cwd=cwd, verbose=False).output()
+    outputs = PretfProxy(cwd=path, verbose=False).output()
 
     values = {}
     for name, data in outputs.items():
